4174f1f7796d2c0477fe09f2ab22af7e4d39e1b8
M SYAYID AS SHIDIQI <msyayidasshidiqi@gmail.com>
2026-09-21 17:44:48 +0700

    Improve lib dan service
    imporve axios & categorServices

--- frontend/src/services/categoryService.js
+++ frontend/src/services/categoryService.js
@@ -5,7 +5,7 @@ const categoryService = {
     getById: (id) => api.get(`/categories/${id}`),
     create: (data) => api.post("/categories", data),
     update: (id, data) => api.put(`/categories/${id}`, data),
-    remove: (id) => api.delete(`/categories/${id}`),
+    delete: (id) => api.delete(`/categories/${id}`),
 };
 
 export default categoryService;
